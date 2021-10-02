e2c1396b40743245147d1a6f25148474d59afdc4
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-10-02 15:35:33 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {    
+    clown_nose = loadImage('https://i.postimg.cc/13Pf4htW/clown-nose.jpg');
 }
 
 function setup() {
@@ -21,6 +25,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
@@ -28,6 +34,7 @@ function gotPoses(results)
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
